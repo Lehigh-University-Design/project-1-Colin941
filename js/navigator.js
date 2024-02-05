b6ac6723f0399c73716a2fd727f137a79a68bc88
Colin941 <coh225@lehigh.edu>
2024-02-05 09:30:44 -0500

    working links

--- js/navigator.js
+++ js/navigator.js
@@ -4,18 +4,18 @@
 var websites = [
     
   
-    https://lehigh-university-design.github.io/project-1-awb324/
-    https://lehigh-university-design.github.io/project-1-sit225/
-    https://lehigh-university-design.github.io/project-1-adrawoot/
-    https://lehigh-university-design.github.io/project-1-mmurata22/
-    https://lehigh-university-design.github.io/project-1-SSShee77y/
-    https://lehigh-university-design.github.io/project-1-eyt225/”
-    https://lehigh-university-design.github.io/project-1-shirls326/”
-    https://lehigh-university-design.github.io/project-1-Colin941/
-    https://lehigh-university-design.github.io/project-1-ellafall/
-    https://lehigh-university-design.github.io/project-1-laurj46/
-    https://lehigh-university-design.github.io/project-1-kaydolan/
-    https://lehigh-university-design.github.io/project-1-dsw225/
+    "https://lehigh-university-design.github.io/project-1-awb324/",
+    "https://lehigh-university-design.github.io/project-1-sit225/",
+    "https://lehigh-university-design.github.io/project-1-adrawoot/",
+    "https://lehigh-university-design.github.io/project-1-mmurata22/",
+    "https://lehigh-university-design.github.io/project-1-SSShee77y/",
+    "https://lehigh-university-design.github.io/project-1-eyt225/",
+    "https://lehigh-university-design.github.io/project-1-shirls326/",
+    "https://lehigh-university-design.github.io/project-1-Colin941/",
+    "https://lehigh-university-design.github.io/project-1-ellafall/",
+    "https://lehigh-university-design.github.io/project-1-laurj46/",
+    "https://lehigh-university-design.github.io/project-1-kaydolan/",
+    "https://lehigh-university-design.github.io/project-1-dsw225/",
   		
      
 ];
